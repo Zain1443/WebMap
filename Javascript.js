edc20c84990c63e90c0f4de57157ee6ce368f68e
Zain Ul Abdin Siyal <113477680+Zain1443@users.noreply.github.com>
2023-11-14 20:08:43 -0600

    Update Javascript.js

--- Javascript.js
+++ Javascript.js
@@ -4,7 +4,7 @@ require([
 ], (MapView, WebMap) => {
   const webmap = new WebMap({
     portalItem: {
-      id: "d4a2e2c8ea9f496187b7311f973421a6"
+      id: "ce2d788da76e4ffdb68063a8c24aed0b"
     },
     basemap: "gray" // Change the basemap to "Gray"
   });
